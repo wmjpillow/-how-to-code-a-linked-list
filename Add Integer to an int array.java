9080697af1fa2128e084345f0ac0c5bbfed1a5b1
Meijie Wang <43675987+wmjpillow@users.noreply.github.com>
2019-10-05 21:42:23 -0400

    Update Add Integer to an int array.java

--- Add Integer to an int array.java
+++ Add Integer to an int array.java
@@ -1,5 +1,10 @@
 //https://stackoverflow.com/questions/29172164/adding-integers-to-an-int-array
 
+// List<Integer> number= new ArrayList<>();
+// The benefit of using ArrayList is that we could use .add() and .remove() method to change element of the array easily.
+// compared to
+// int[] number= new int[n];
+
 
 int[] num= new int[args.length];
 for(int i=0;i<args.length;i++){
