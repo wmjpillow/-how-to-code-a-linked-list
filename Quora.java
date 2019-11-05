4a4ced02ab3ff5cadae773ee17cf74f4e9dca883
Meijie Wang <43675987+wmjpillow@users.noreply.github.com>
2019-11-05 10:32:48 -0500

    Update Quora.java

--- Quora.java
+++ Quora.java
@@ -176,7 +176,50 @@ public static int divideSubString( String s, int k){
     return res;
 }
 
+//remove Exact One Digit
+public static int removeExactOneDigit(String s1, String s2){
+    if(s1==null && s2== null){return 0;}
+    if(s1== null){return s2.length();}
+    if(s2==null){return 0;}
+    if(s1.length()==0 && s2.length()==0){return 0;}
+    if(s1.length()==0){return s2.length();}
+    else if(s2.length()==0){return 0;}
+    int len1= s1.length();
+    int len2= s2.length();
+    int s1Arrow= 0;
+    int s2Arrow= 0;
+    int res = 0;
+    if(s1.charAt(0)<s2.charAt(0)){
+        res=(s1.length()-1)+(s2.length()-1);
+        if(compareStringVal(s1.substring(1),s2)>0){res++;}
+        if(compareStringVal(s1,s2.substring(1))>0){res++;}
+    }
+    else if(s1.charAt(s1Arrow)==s2.charAt(s2Arrow)){
+        res=removeExactOneDigit(s1.substring(1),s2.substring(1));
+        if(compareStringVal(s1.substring(1),s2)>0){res++;}
+        if(compareStringVal(s1,s2.substring(1))>0){res++;}
+    }
+    else{if(compareStringVal(s1.substring(1),s2)>0){res=1;}
+        else{ res=0;}}
+    return res;
+    }
 
+public static int compareStringVal(String s1, String s2){
+    int len1= s1.length();
+    int len2= s2.length();
+    int s1Arrow= 0;
+    int s2Arrow= 0;
+    while(s1Arrow< len1 && s2Arrow<len2){
+        if(s1.charAt(s1Arrow)<s2.charAt(s2Arrow)){
+            return 1;
+        } else if(s1.charAt(s1Arrow)== s2.charAt(s2Arrow)){
+            s1Arrow++;
+            s2Arrow++;
+        }else { return -1;}
+    }
+    if(s1Arrow<len1){return -1;}
+    else{return 1;}
+}
 
 
 
