e2125edf67c1fd9a86e9415cd86dff3a0dd723a2
Meijie Wang <43675987+wmjpillow@users.noreply.github.com>
2019-11-09 04:47:30 -0500

    Update Quora.java

--- Quora.java
+++ Quora.java
@@ -268,4 +268,58 @@ public static void diagonalsSort(int[][] matrix){
 }
 
 
+//Find Min in Array
+public static List<Integer> findMin(int m, int n, int[][] queries){
+    //create matrix and create map of value
+    TreeMap<Integer,Integer> map= new TreeMap<Integer,Integer>();
+    int[][] matrix= new int[m][n];
+    for(int i=0;i<m;i++){
+        for(int j=0; j<n; j++){
+            matrix[i][j]=(i+1)*(j+1);
+            map.put(matrix[i][j],map.getOrDefault(matrix[i][j])+1);
+        }
+    }
+    List<Integer> res= new ArrayList<>();
+    for(int[] query: queries){
+        if(query.length==1){
+            for(int key: map.keySet()){
+                if(map.get(key)!=0){
+                    res.add(key);
+                    break;
+                }
+            }
+        }
+        else if(query.length==2){
+            if(query[0]==1){
+                int row=query[1];
+                for(int j=0; j<n; j++){
+                    if(matrix[row][j]!= -1 && map.containsKey(matrix[row][j])){
+                        map.put(matrix[row][j],map.getOrDefault(matrix[row][j]+1));
+                        matrix[row][j]= -1;
+                    }
+                }
+            } else{
+                int col =query[1];
+                for(int i=0; i<m; i++){
+                    if(matrix[i][col]!=-1 && map.containsKey(matrix[i][col])){
+                        map.put(matrix[i][col],map.getOrDefault(matrix[i][col]+1));
+                        matrix[i][col]=-1;
+                    }
+                }
+            }
+        }
+    }
+    return res;
+}
+
+public static void print2D(int[][] matrix){
+    int m= matrix.length;
+    int n= matrix[0].length;
+    for(int i= 0; i<m ;i++){
+        for(int j=0; j<n; j++){
+            System.out.print(matrix[i][j]);
+        }
+        System.out.println();
+    }
+}
 
