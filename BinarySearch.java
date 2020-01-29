b2d847b7c0afe64a2fe50195c40e778022a46a59
Meijie Wang <43675987+wmjpillow@users.noreply.github.com>
2020-01-29 09:30:38 -0500

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -31,3 +31,12 @@ Public static int binarySearch( int[] arr, int start, int end, int hkey){
     }
     return result;
 }
+
+
+//https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/discuss/85173/Share-my-thoughts-and-Clean-Java-Code
+// The key point for any binary search is to figure out the "Search Space". For me, I think there are two kind of "Search Space" -- index and range(the range from the smallest number to the biggest number). Most usually, when the array is sorted in one direction, we can use index as "search space", when the array is unsorted and we are going to find a specific number, we can use "range".
+
+// Let me give you two examples of these two "search space"
+
+// index -- A bunch of examples -- https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/ ( the array is sorted)
+// range -- https://leetcode.com/problems/find-the-duplicate-number/ (Unsorted Array)
